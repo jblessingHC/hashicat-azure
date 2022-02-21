b42f30014d68a7adcff1b637aaf604645a3346ad
jblessingHC <85644461+jblessingHC@users.noreply.github.com>
2022-02-21 16:21:40 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
-module "network" {
-  source  = "app.terraform.io/jblessing-training/network/azurerm"
-  version = "3.5.0"
+#module "network" {
+#  source  = "app.terraform.io/jblessing-training/network/azurerm"
+#  version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-}
+#  resource_group_name = azurerm_resource_group.myresourcegroup.name
+#}
