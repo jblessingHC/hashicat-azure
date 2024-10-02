2e811aacfb2ed19218b00c554f513aa6e491772c
jblessingHC <85644461+jblessingHC@users.noreply.github.com>
2024-10-02 12:12:58 -0400

    Update outputs.tf
    adding resource count

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "catapp_url" {
 output "catapp_ip" {
   value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
 }
+
+
+output "resource_count" {
+  value = length(data.terraform_remote_state.example.outputs.resources)
+}
